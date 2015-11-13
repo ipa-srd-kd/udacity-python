52ea8c6e931ba687b8c1f120c955b286a2b44216
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-13 14:54:02 +0100

    change duration of sleep

--- make_break.py
+++ make_break.py
@@ -6,6 +6,6 @@ break_total = 3
 break_count = 0
 print("This is it: "+time.ctime())
 while(break_count <break_total):
-  time.sleep(2)
+  time.sleep(20)
   webbrowser.open("https://trello.com/b/VcXlGp4b/srd-kd")
   break_count = break_count + 1
