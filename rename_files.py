e3b025c5ac6deca269d592bc719bcf4ae4006606
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-13 15:56:48 +0100

    change phot name with program

--- rename_files.py
+++ rename_files.py
@@ -3,9 +3,13 @@ import os
 def rename_files():
   # git filenames os.listdir()
   file_list = os.listdir("/home/srd-kd/Downloads/udacity/python/rename_photos_exercise")
-  print(file_list)
+  #print(file_list)
+  path=os.getcwd()
+  print(path)
   # rename
-
-
+  os.chdir("/home/srd-kd/Downloads/udacity/python/rename_photos_exercise")
+  for file_name in file_list:
+    os.rename(file_name,file_name.translate(None,"0123456789"))
+  os.chdir(path)
 
 rename_files()
