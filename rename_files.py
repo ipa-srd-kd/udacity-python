0c0f3e67d3a17405878376ca42821ac06c3fddc5
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-13 15:39:32 +0100

    fix spelling error

--- rename_files.py
+++ rename_files.py
@@ -2,7 +2,8 @@ import os
 
 def rename_files():
   # git filenames os.listdir()
-  os.listdir("/home/srd-kd/Downloads/udacity/python")
+  file_list = os.listdir("/home/srd-kd/Downloads/udacity/python/rename_photos_exercise")
+  print(file_list)
   # rename
 
 
