151c26caa7fc8f241e60ebf80e9f33512399fc0d
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-13 14:44:49 +0100

    add while loop to break program

--- make_break.py
+++ make_break.py
@@ -1,1 +1,11 @@
+import webbrowser
+import time
+
 print "Hello World"
+break_total = 3
+break_count = 0
+print("This is it: "+time.ctime())
+while(break_count <break_total):
+  time.sleep(2)
+  webbrowser.open("https://trello.com/b/VcXlGp4b/srd-kd")
+  break_count = break_count + 1
