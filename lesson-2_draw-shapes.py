68b9425ae1dcce17d9715a843dcc1836f24c9ddb
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-16 10:22:53 +0100

    draw rectangular with turtle

--- lesson-2_draw-shapes.py
+++ lesson-2_draw-shapes.py
@@ -5,6 +5,13 @@ def draw_square():
   window.bgcolor("red")
   brad = turtle.Turtle()
   brad.forward(100)
+  brad.right(90)
+  brad.forward(100)
+  brad.right(90)
+  brad.forward(100)
+  brad.right(90)
+  brad.forward(100)
+  brad.right(90)
   
   window.exitonclick()
   
