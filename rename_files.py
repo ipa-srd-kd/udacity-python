c9d87c96dbc16960340025048cab5d160719a236
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-16 09:14:14 +0100

    fix renaming folder

--- rename_files.py
+++ rename_files.py
@@ -2,12 +2,12 @@ import os
 
 def rename_files():
   # git filenames os.listdir()
-  file_list = os.listdir("/home/srd-kd/Downloads/udacity/python/rename_photos_exercise")
+  file_list = os.listdir("/home/srd-kd/Downloads/udacity/python/lesson-1_rename-prank/")
   #print(file_list)
   path=os.getcwd()
   print(path)
   # rename
-  os.chdir("/home/srd-kd/Downloads/udacity/python/rename_photos_exercise")
+  os.chdir("/home/srd-kd/Downloads/udacity/python/lesson-1_rename-prank/")
   for file_name in file_list:
     os.rename(file_name,file_name.translate(None,"0123456789"))
   os.chdir(path)
