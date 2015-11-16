206e2d2de83b53df040f31f81e0589bf48b908d0
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-16 14:09:33 +0100

    insert circle of circle

--- lesson-2_draw-shapes.py
+++ lesson-2_draw-shapes.py
@@ -1,24 +1,14 @@
 import turtle
 
 def draw_square():
-  brad = turtle.Turtle()
-  brad.shape("turtle")
-  brad.color("blue","red")
-  brad.speed(2)
   for i in range(0,4):
     brad.forward(100)
     brad.right(90)
       
 def draw_circle():
-  angie = turtle.Turtle()
-  angie.shape("arrow")
-  angie.color("blue")
   angie.circle(100)
 
 def draw_triangle():
-  lucy = turtle.Turtle()
-  lucy.shape("circle")
-  lucy.color("green")
   for i in range(0,3):
     lucy.right(120)
     lucy.forward(100)
@@ -26,10 +16,29 @@ def draw_triangle():
 
 window = turtle.Screen()
 window.bgcolor("red")
+brad = turtle.Turtle()
+brad.shape("turtle")
+brad.color("blue","red")
+brad.speed(100)
 
-draw_square() 
-draw_circle()
-draw_triangle()
+angie = turtle.Turtle()
+angie.shape("arrow")
+angie.color("blue")
+angie.speed(100)
+  
+lucy = turtle.Turtle()
+lucy.shape("circle")
+lucy.color("green")
+
+#for i in range(0,50):
+  #draw_square()
+  #brad.right(10) 
+  
+for i in range(0,500):
+  draw_circle()
+  angie.right(2)
+  angie.forward(2)
+#draw_triangle()
 
 window.exitonclick()
 
