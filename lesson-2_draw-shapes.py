75840e1a8271f63152372a14e2237016f57488bc
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-16 13:22:42 +0100

    add lucy who draws a triangle

--- lesson-2_draw-shapes.py
+++ lesson-2_draw-shapes.py
@@ -1,27 +1,35 @@
 import turtle
 
 def draw_square():
-  window = turtle.Screen()
-  window.bgcolor("red")
   brad = turtle.Turtle()
   brad.shape("turtle")
   brad.color("blue","red")
   brad.speed(2)
-  
-  brad.forward(100)
-  brad.right(90)
-  brad.forward(100)
-  brad.right(90)
-  brad.forward(100)
-  brad.right(90)
-  brad.forward(100)
-  brad.right(90)
-  
+  for i in range(0,4):
+    brad.forward(100)
+    brad.right(90)
+      
+def draw_circle():
   angie = turtle.Turtle()
   angie.shape("arrow")
   angie.color("blue")
   angie.circle(100)
+
+def draw_triangle():
+  lucy = turtle.Turtle()
+  lucy.shape("circle")
+  lucy.color("green")
+  for i in range(0,3):
+    lucy.right(120)
+    lucy.forward(100)
   
-  window.exitonclick()
-  
-draw_square(); 
+
+window = turtle.Screen()
+window.bgcolor("red")
+
+draw_square() 
+draw_circle()
+draw_triangle()
+
+window.exitonclick()
+
