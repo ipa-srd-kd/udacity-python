7126acae2f3178190a0a421d84c6d8fd33218a89
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-16 11:08:45 +0100

    change color, shape and speed

--- lesson-2_draw-shapes.py
+++ lesson-2_draw-shapes.py
@@ -4,6 +4,10 @@ def draw_square():
   window = turtle.Screen()
   window.bgcolor("red")
   brad = turtle.Turtle()
+  turtle.shape("circle")
+  turtle.color("green","red")
+  turtle.speed(10)
+  
   brad.forward(100)
   brad.right(90)
   brad.forward(100)
