4f3e8d3383004c3f4bdd0294f8d8fc00b316429f
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-18 10:15:01 +0100

    add new inheritance

--- lesson-x_python-functions/function.py
+++ lesson-x_python-functions/function.py
@@ -62,3 +62,18 @@ mpt.x, mpt.y = 3, 4
 print "Cartesian: "+str(cpt.distanceToOrigin())
 print "Manhattan: "+str(mpt.distanceToOrigin())
 
+# smarter:
+
+class point:
+  def __init__(self, x=0, y=0):
+    self.x, self.y = x, y
+    
+class cartesian(point):
+  def distanceToOrigin(self):
+    return floor(sqrt(self.x**2 + self.y**2))
+class manhattan(point):
+  def distanceToOrigin(self):
+    return abs(self.x) + abs(self.y)
+    
+    
+
