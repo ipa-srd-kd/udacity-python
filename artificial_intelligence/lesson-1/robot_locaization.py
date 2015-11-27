e2ef0a1c6e1444806c87a9af7e4db7e7aefa1928
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-11-27 10:16:41 +0100

    add observation, initial distribution and first update (with normalization)

--- artificial_intelligence/lesson-1/robot_locaization.py
+++ artificial_intelligence/lesson-1/robot_locaization.py
@@ -19,33 +19,38 @@ import sys
 
 def initial_probability(n):
   assert n > 0
-  probability_vector = []
+  probability_vect = []
   for i in range(0, n):
-    probability_vector.append(1.0/n)
-  assert (len(probability_vector) == n)
-  assert ((sum(probability_vector) - 1) <= sys.float_info.epsilon)
-  return probability_vector
+    probability_vect.append(1.0/n)
+  assert (len(probability_vect) == n)
+  assert (abs(sum(probability_vect) - 1.0) <= sys.float_info.epsilon)
+  return probability_vect
 
 def update(probability_vect, observation_vect):
   assert len(probability_vect) == len(observation_vect)
   n = len(probability_vect)
   for i in range(0, n):
     if observation_vect[i] == "green":
-      probability_vect[i] = probability_vect[i] * 0.6
-    elif observation_vect[i] == "red":
       probability_vect[i] = probability_vect[i] * 0.2
+    elif observation_vect[i] == "red":
+      probability_vect[i] = probability_vect[i] * 0.6
     else:
       probability_vect[i] = probability_vect[i] * 0.2
+  # normalize
+  probability_sum = sum(probability_vect)
+  for i in range(0, n):
+    probability_vect[i] = probability_vect[i]/probability_sum
+  assert (abs(sum(probability_vect) - 1.0) <= sys.float_info.epsilon)
   return probability_vect
     
 
 
 observations = ["green", "red", "red", "green", "green"]
-print observations
+print("Observation:    "+str(observations))
 n = len(observations)
-print n
+print("number of Cell: "+str(n))
 probabilities = initial_probability(n)
-print "Initial: "+str(probabilities)
+print("Initial:        "+str(probabilities))
 probabilities = update(probabilities, observations)
-print "First Update: "+str(probabilities)      
+print("1. Update:      "+str(probabilities))      
 
