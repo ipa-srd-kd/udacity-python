450b9db47a9cff8d9f2e273d8a2c0029957a049d
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-12-04 15:51:26 +0100

    add buggy code again

--- artificial_intelligence/lesson-1/robot_localization_2d.py
+++ artificial_intelligence/lesson-1/robot_localization_2d.py
@@ -90,12 +90,24 @@ def move(p_matrix, mv, p_move):
   assert type(mv[1]) == int
   #assert (abs(sum(p_matrix) - 1.0) <= eps)
   
-  for i in range(len(p_matrix)):
-    for j in range(len(p_matrix[0])):
-      s =     p_matrix[i-mv[0]][j-mv[1]] % len(p_matrix[i]) * p_move
-      s = s + p_matrix[i][j] % len(p_matrix[i]) * (1.0 - p_move)      
-      p_matrix[i][j] = s
-  
+  if(mv[0] != 0):
+    for i in range(len(p_matrix)):
+      for j in range(len(p_matrix[0])):
+        print "i: ",i," j: ",j," mv0:",mv[0]," val: ",p_matrix[i-mv[0]][j]
+        s =     p_matrix[i-mv[0]][j] % len(p_matrix) * p_move
+        print s
+        s = s + p_matrix[i][j] % len(p_matrix) * (1.0 - p_move)      
+        print s
+        p_matrix[i][j] = s
+  elif(mv[1] != 0):
+    for i in range(len(p_matrix)):
+      for j in range(len(p_matrix[i])):
+        print "i: ",i," j: ",j," mv1:",mv[1]," j: ",(j-mv[1])% len(p_matrix[i])," val: ",p_matrix[i][(j-mv[1])% len(p_matrix[i])]
+        s =     p_matrix[i][(j-mv[1])% len(p_matrix[i])]  * p_move
+        print s
+        s = s + p_matrix[i][j] % len(p_matrix[i]) * (1.0 - p_move)      
+        print s
+        p_matrix[i][j] = s
   #res_vect = normalize(res_vect)
   #assert len(res_vect) == len(p_vect)
   #probability_assertion(res_vect)
