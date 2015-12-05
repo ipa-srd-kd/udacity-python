1f183e322515ad8ad9920291809fc3cf46a82504
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-12-05 18:06:58 +0100

    add some minor comments

--- artificial_intelligence/lesson-2/kalman_filter.py
+++ artificial_intelligence/lesson-2/kalman_filter.py
@@ -5,25 +5,33 @@
 # large variance => thin spread distribution  => less uncertain
 
 import math
-# first calculation with a kalman filter
+################
+### kalman function
+################
 def kalman(x, mu, sigma_squared):
   y = (1.0 / math.sqrt(2.0 * math.pi * sigma_squared)
        * math.exp(-0.5 * (math.pow(x - mu, 2.0) / sigma_squared)))
   return y
 
-# SENSING UPDATE (update): updated gaussian from two gaussians (measurement update step)
+################
+### SENSING UPDATE (update): updated gaussian from two gaussians (measurement update step)
+################
 def sensor_update(mean1, var1, mean2, var2):
   new_mean = (1.0 / (var1 + var2)) * ((var2 * mean1) + (var1 * mean2))
   new_var = 1.0 / ((1.0 /var1) + (1.0 /var2))
   return [new_mean, new_var]
 
-# MOTION UPDATE (prediciton):
+################
+### MOTION UPDATE (prediciton):
+################
 def motion_update(mean1, var1, mean2, var2):
   new_mean = mean1 + mean2
   new_var = var1 + var2   
   return [new_mean, new_var]
     
-#main
+###########################    
+### Test Kalmanfunction ###
+###########################
 """
 ## Test Kalmanfilter 
 x = 8
@@ -31,12 +39,17 @@ mu = 10
 sigma_squared = 4
 print kalman(x, mu, sigma_squared)
 """
+#####################################
+### Test sensor and motion update ###
+#####################################
 """
-## Test sensor and motion update
 print sensor_update(10., 8., 13., 2.)
 print motion_update(8., 4., 10., 6.)
 """
 
+########################################
+### Example of FULL 1D kalman filter ###
+########################################
 measurements = [5., 6., 7., 9., 10.]
 motion = [1., 1., 2., 1., 1.]
 measurement_sig = 4.
@@ -44,14 +57,11 @@ motion_sig = 2.
 mu = 0.
 sig = 10000.
 
-#Please print out ONLY the final values of the mean
-#and the variance in a list [mu, sig]. 
 update = sensor_update
 predict = motion_update
 
-# Insert code here
+
 for i in range(len(measurements)):
     [mu, sig] = update(mu, sig, measurements[i], measurement_sig)
     [mu, sig] = predict(mu, sig, motion[i], motion_sig)
-
 print [mu, sig]
