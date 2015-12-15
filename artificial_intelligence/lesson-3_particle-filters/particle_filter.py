78bed3c8862eb52f0d32c66a43dd469ae3622eea
srd-kd <david.kudlek@ipa.fraunhofer.de>
2015-12-15 07:50:13 +0100

    fishish first exercise

--- artificial_intelligence/lesson-3_particle-filters/particle_filter.py
+++ artificial_intelligence/lesson-3_particle-filters/particle_filter.py
@@ -114,6 +114,25 @@ def eval(r, p):
 
 ####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
 
+##############
+### TASK 1 ###
+##############
+# Make a robot called myrobot that starts at
+# coordinates 30, 50 heading north (pi/2).
+# Have your robot turn clockwise by pi/2, move
+# 15 m, and sense. Then have it turn clockwise
+# by pi/2 again, move 10 m, and sense again.
+#
+# Your program should print out the result of
+# your two sense measurements.
+
 myrobot = robot()
+myrobot.set(30,50,pi/2)
+myrobot = myrobot.move(-pi/2,15.0)
+z_one = myrobot.sense()
+print z_one
+myrobot = myrobot.move(-pi/2,10.0)
+z_two = myrobot.sense()
+print z_two
 
 
